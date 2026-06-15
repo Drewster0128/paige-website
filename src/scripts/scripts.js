13d564de3b89c03f0b420458639ec80a6a6911cc
Drewster0128 <acook21@hawk.iit.edu>
2026-06-15 10:42:19 -0500

    Add getImagesOnDrive function

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,13 +1,17 @@
 import { google } from "googleapis"
-import { promises } from "fs"
+import { promises, createWriteStream} from "fs"
+import { Writable } from "stream";
 
-const AUTH = new google.auth.GoogleAuth({
-    keyFile: process.env.KEYFILE,
-    scopes: [process.env.SCOPES]
+const auth = new google.auth.GoogleAuth({
+    keyFile: 'env/paigewebsite-c02d0030e691.json',
+    scopes: ['https://www.googleapis.com/auth/drive.readonly']
 });
+
+
 
 // fetch meta-data from google drive
 async function getMetaData() {
+
     let sheets = google.sheets({
         version: 'v4',
         auth
@@ -38,3 +42,30 @@ async function getMetaData() {
 async function saveMetaData(metadata, filename) {
     await promises.writeFile(filename, metadata, 'utf8');
 }
+
+async function getImagesOnDrive() {
+    let drive = google.drive({
+        version: 'v3',
+        auth
+    });
+
+    let response = await drive.files.list({
+        q: `'${process.env.WEBSITE_IMAGES_FOLDER_ID}' in parents`
+    });
+
+    response = response.data.files;
+    
+    //for each file in response, download a blob representation
+    for(let image of response) {
+        let imageBlob = await drive.files.get({
+            fileId: image.id,
+            alt: 'media'
+        });
+
+        let writableStream = Writable.toWeb(createWriteStream(`public/img/website_images/${image.name}`));
+        imageBlob.data.stream().pipeTo(writableStream);
+    }
+}
+
+//getMetaData()
+getImagesOnDrive()
