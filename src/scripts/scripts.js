978ed5db79f4ad2025db5373c21f55ee732baa90
Drewster0128 <acook21@hawk.iit.edu>
2026-06-15 08:58:17 -0500

    Add getMetaData function

---
+++ src/scripts/scripts.js
@@ -0,0 +1,34 @@
+import { google } from "googleapis"
+
+const AUTH = new google.auth.GoogleAuth({
+    keyFile: process.env.KEYFILE,
+    scopes: [process.env.SCOPES]
+});
+
+// fetch meta-data from google drive
+async function getMetaData() {
+    let sheets = google.sheets({
+        version: 'v4',
+        auth
+    });
+
+    let response = await sheets.spreadsheets.values.get({
+        spreadsheetId: process.env.SPREADSHEET_ID,
+        range:"Sheet1"
+    });
+
+    response = response.data.values;
+
+    let columnNames = response[0];
+    let temp = [];
+
+    response.slice(1).forEach((row, rowIndex) => {
+        temp[rowIndex] = {};
+        columnNames.forEach((column, columnIndex) => {
+            temp[rowIndex][column] = row[columnIndex];
+        })
+    });
+
+    let jsonObject = JSON.stringify(temp);
+    return jsonObject;
+}
