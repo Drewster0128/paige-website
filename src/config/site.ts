33f03879dba767f70d07753c197d967b69abb732
Drewster0128 <acook21@hawk.iit.edu>
2026-08-19 16:07:50 -0500

    Use new tally form url

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const SITE_BRAND_NAME = "Psychedelic Queen Artistry";
 export const SITE_ARTIST_NAME = "Paige Cook";
-export const GENERAL_CONTACT_EMAIL = "paige@example.com";
+export const GENERAL_CONTACT_EMAIL = "psychdelic.queen.pc@gmail.com";
 
 export const DEFAULT_METADATA_IMAGE = "/img/home/hero.webp";
 export const DEFAULT_METADATA_IMAGE_ALT = "Paige Cook painting in her studio";
